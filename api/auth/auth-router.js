f5aae8e68fd92b782e0db81e45992bb255ad7a78
DJcri <djcri123@gmail.com>
2021-04-20 17:58:00 -0700

    completed MVP

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require("bcryptjs");
+const Users = require("../users/users-model");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -14,10 +17,20 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  user.password = hash;
+
+  try {
+    const saved = Users.add(user);
+    res.status(201).json(saved);
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-router.post("/login", checkUsernameExists, (req, res, next) => {
+router.post("/login", checkUsernameExists, async (req, res, next) => {
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -37,6 +50,30 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username })[0];
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = makeToken(user);
+      res
+        .status(200)
+        .json({ message: `${user.username} is back!`, payload: token });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
+
+const makeToken = (user) => {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name,
+  };
+  const options = {
+    expiresIn: "500s",
+  };
+  jwt.sign(payload, JWT_SECRET, options);
+};
 
 module.exports = router;
